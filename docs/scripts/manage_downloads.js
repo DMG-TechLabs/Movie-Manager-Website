6370889f0518a26c81233c32fb53601e318b19e0
KDesp <kdesp2003@gmail.com>
2023-02-26 21:36:56 +0200

    download page functionality done

---
+++ docs/scripts/manage_downloads.js
@@ -0,0 +1,34 @@
+const container = document.getElementById("container");
+const programName = "Movie-Manager";
+
+function createDownload(json) {
+  let listItem = document.createElement("li");
+  let link = document.createElement("a");
+  let span = document.createElement("span");
+  let div = document.createElement("div");
+  let version = document.createElement("p");
+
+  div.classList.add("download-link");
+  span.classList.add("date");
+  listItem.classList.add("downloads-item");
+
+  version.innerHTML = "v" + json.version;
+  link.href = "";
+  link.setAttribute("download", json.location);
+  link.download = programName + "_v" + json.version + ".jar";
+  span.innerHTML = "Uploaded: " + json.date;
+
+  div.appendChild(version);
+  link.appendChild(div);
+  listItem.appendChild(link);
+  listItem.appendChild(span);
+
+  container.appendChild(listItem);
+}
+
+function loadDownloads() {
+  for (let i = 0; i < versions.length; i++) {
+    createDownload(versions[i]);
+    console.log(versions[i]);
+  }
+}
---
+++ docs/scripts/versions.js
@@ -0,0 +1,12 @@
+let versions = [
+    {
+        version: "1.0.1",
+        location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
+        date: "2023/01/21"
+    },
+    {
+        version: "1.0.0",
+        location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
+        date: "2022/11/10"
+    }
+]
