44895a39a8d0077ac48bc9a43e3e7cda6fd820c1
KDesp <kdesp2003@gmail.com>
2023-03-06 16:04:17 +0200

    changed width height

--- docs/scripts/versions.js
+++ docs/scripts/versions.js
@@ -2,11 +2,13 @@ let versions = [
     {
         version: "1.0.1",
         location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
-        date: "2023/01/21"
+        date: "2023/01/21",
+        log: ""
     },
     {
         version: "1.0.0",
         location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
-        date: "2022/11/10"
+        date: "2022/11/10",
+        log: ""
     }
 ]
