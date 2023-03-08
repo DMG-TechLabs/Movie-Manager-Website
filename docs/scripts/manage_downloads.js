52c4cf67cbfdec0177791eed0efe149c66dde6a1
KDesp <kdesp2003@gmail.com>
2023-03-08 17:41:29 +0200

    Change log added to downloads

--- docs/scripts/manage_downloads.js
+++ docs/scripts/manage_downloads.js
@@ -7,21 +7,29 @@ function createDownload(json) {
     let span = document.createElement("span");
     let div = document.createElement("div");
     let version = document.createElement("p");
+    let log = document.createElement("div");
+    let changes = document.createElement("p");
 
     div.classList.add("download-link");
     span.classList.add("date");
     listItem.classList.add("downloads-item");
+    log.classList.add("change-log");
+
 
     version.innerHTML = "v" + json.version;
     link.href = "";
     link.setAttribute("download", json.location);
     link.download = programName + "_v" + json.version + ".jar";
     span.innerHTML = "Uploaded: " + json.date;
+    changes.innerHTML = "<b>Change log</b>: <br>" + json.log;
 
     div.appendChild(version);
     link.appendChild(div);
+    log.appendChild(changes);
     listItem.appendChild(link);
     listItem.appendChild(span);
+    listItem.appendChild(log);
+
 
     container.appendChild(listItem);
 }
--- docs/scripts/versions.js
+++ docs/scripts/versions.js
@@ -3,12 +3,12 @@ let versions = [
         version: "1.0.1",
         location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
         date: "2023/01/21",
-        log: ""
+        log: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, eius aliquam. Suntexplicabo voluptate ea! Dignissimos ducimus magnam excepturi, saepe vel alias! Veritatismagni fugit laudantium adipisci enim necessitatibus repellendus?"
     },
     {
         version: "1.0.0",
         location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
         date: "2022/11/10",
-        log: ""
+        log: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, eius aliquam. Suntexplicabo voluptate ea! Dignissimos ducimus magnam excepturi, saepe vel alias! Veritatismagni fugit laudantium adipisci enim necessitatibus repellendus?"
     }
 ]
