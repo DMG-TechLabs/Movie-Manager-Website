506706e878fc4e27aafcccfaecb4e57777bd67d0
KDesp <kdesp2003@gmail.com>
2023-03-20 20:02:51 +0200

    first release added

--- docs/scripts/versions.js
+++ docs/scripts/versions.js
@@ -1,14 +1,8 @@
 let versions = [
     {
-        version: "1.0.1",
-        location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
-        date: "2023/01/21",
-        log: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, eius aliquam. Suntexplicabo voluptate ea! Dignissimos ducimus magnam excepturi, saepe vel alias! Veritatismagni fugit laudantium adipisci enim necessitatibus repellendus?"
-    },
-    {
         version: "1.0.0",
-        location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/MovieManager-1-SNAPSHOT.jar",
-        date: "2022/11/10",
-        log: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, eius aliquam. Suntexplicabo voluptate ea! Dignissimos ducimus magnam excepturi, saepe vel alias! Veritatismagni fugit laudantium adipisci enim necessitatibus repellendus?"
+        location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/Watchlist Wizard.zip",
+        date: "2023/03/20",
+        log: "The program features:<br>* TMDb api<br>* Play movies from selected media player<br>* Change font / theme<br>* Create theme<br>* Select preferred extensions<br>* Edit movie information<br>* Sort / Search"
     }
 ]
