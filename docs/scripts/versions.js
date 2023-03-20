eeb8192c6d06642bf07074e8a36162f0df5ed5fb
KDesp <kdesp2003@gmail.com>
2023-03-20 22:07:17 +0200

    version rename

--- docs/scripts/versions.js
+++ docs/scripts/versions.js
@@ -1,7 +1,7 @@
 let versions = [
     {
         version: "1.0.0",
-        location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/Watchlist Wizard.zip",
+        location: "https://users.iee.ihu.gr/~iee2021035/mm-versions/Watchlist_Wizard_v1.0.0.zip",
         date: "2023/03/20",
         log: "The program features:<br>* TMDb api<br>* Play movies from selected media player<br>* Change font / theme<br>* Create theme<br>* Select preferred extensions<br>* Edit movie information<br>* Sort / Search"
     }
